7ed1d467ac1cbe9d56cfc661906d4cbc5a553033
ericdjavid <edjavid@student.42.fr>
2021-07-09 15:54:55 +0200

    working on the norm

--- inc/so_long.h
+++ inc/so_long.h
@@ -35,39 +35,40 @@ typedef struct s_coord
 typedef struct s_game
 {
 	t_mlx	mlx;
-	int	map_width;
-	int	map_height;
-	int	numb_move;
+	int		map_width;
+	int		map_height;
+	int		numb_move;
 	t_img	*ground;
 	t_img	*player;
 	t_img	*collectible;
 	t_img	*tree;
 	t_img	*exit;
 	char	*map;
-	int	total_line_char;
-	int	line_number;
+	int		total_line_char;
+	int		line_number;
 	char	*player_position;
-	int	nb_exit;
+	int		nb_exit;
+	int		numb;
 }	t_game;
 
-void		texture_load(t_game *game, t_img **img, char *path);
-void		check_arg(int argc, char **argv);
-int		key_hook(int keycode, t_game *game);
-void		check_walls(char *line);
-void		check_map_elements(char *whole_chars);
-int		init_struc(t_game *game);
-int		init_map(t_game *game, char *map_name);
-void		draw_map(t_game *game);
-void		draw_square(t_game *game, t_img *img, int x, int y);
+void			texture_load(t_game *game, t_img **img, char *path);
+void			check_arg(int argc, char **argv);
+int				key_hook(int keycode, t_game *game);
+void			check_walls(char *line);
+void			check_map_elements(char *whole_chars);
+int				init_struc(t_game *game);
+int				init_map(t_game *game, char *map_name);
+void			draw_map(t_game *game);
+void			draw_square(t_game *game, t_img *img, int x, int y);
 unsigned int	mlx_get_pixel(t_img *img, int x, int y);
-void		mlx_draw_pixel(t_img *mlx_img, int x, int y, int color);
+void			mlx_draw_pixel(t_img *mlx_img, int x, int y, int color);
 unsigned int	mlx_rgb_to_int(int o, int r, int g, int b);
-void		check_for_elements(t_game *game, int x, int y, int z, int numb);
-int		exit_hook(t_game *game);
-void		move_bottom(t_game *game);
-void		move_left(t_game *game);
-void		move_right(t_game *game);
-void		move_up(t_game *game);
-void		is_game_finished(t_game *game);
+void			check_for_elements(t_game *game, int x, int y, int z);
+int				exit_hook(t_game *game);
+void			move_bottom(t_game *game);
+void			move_left(t_game *game);
+void			move_right(t_game *game);
+void			move_up(t_game *game);
+void			is_game_finished(t_game *game);
 
 #endif
